a9a07c9fc6142e10e4f030cb2639ae8d226e81ed
Mantasha Fatima <141666680+MantashaFatima@users.noreply.github.com>
2023-10-20 15:18:56 +0530

    jdbjd

---
+++ src/Component/TicTacToe.js
@@ -0,0 +1,96 @@
+import React, { useState } from 'react';
+
+export default function TicTacToe() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+
+    return null;
+  };
+
+  const handleClick = (i) => {
+    const squaresCopy = [...squares];
+
+    if (calculateWinner(squares) || squaresCopy[i]) {
+      return;
+    }
+
+    squaresCopy[i] = xIsNext ? 'X' : 'O';
+    setSquares(squaresCopy);
+    setXIsNext(!xIsNext);
+  };
+
+  const renderSquare = (i) => (
+    <button className="square" onClick={() => handleClick(i)}>
+      {squares[i]}
+    </button>
+  );
+
+  const winner = calculateWinner(squares);
+  const isBoardFull = squares.every((square) => square !== null);
+
+  let status;
+
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else if (isBoardFull) {
+    status = "It's a draw!";
+  } else {
+    status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+  }
+
+  const resetGame = () => {
+    setSquares(Array(9).fill(null));
+    setXIsNext(true);
+  };
+
+  return (
+    <div className="container">
+      <h1>Tic Tac Toe</h1>
+      <div className="centered-box">
+      <div className="box">
+        <div className="board-row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+      </div>
+      </div>
+
+      <div className="game-info">
+        <div>{status}</div>
+        <button className="reset-button" onClick={resetGame}>
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+}
